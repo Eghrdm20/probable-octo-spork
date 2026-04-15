0bc858efe6029df25b70b786e317b794b769a3bf
Eghrdm20 <100941270+Eghrdm20@users.noreply.github.com>
2026-04-15 12:22:48 +0100

    Create page.js
    import Script from "next/script";
    
    export default function RootLayout({ children }) {
      return (
        <html lang="ar">
          <body>
            <Script
              src="https://sdk.minepi.com/pi-sdk.js"
              strategy="beforeInteractive"
            />
            {children}
          </body>
        </html>
      );
    }

---
+++ app/page.js
@@ -0,0 +1,15 @@
+import Script from "next/script";
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ar">
+      <body>
+        <Script
+          src="https://sdk.minepi.com/pi-sdk.js"
+          strategy="beforeInteractive"
+        />
+        {children}
+      </body>
+    </html>
+  );
+}
